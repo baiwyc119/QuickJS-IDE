eaff8c928487dc10000be2a3b52b2923ae2f22d0
本朋 <benpeng.jbp@alibaba-inc.com>
2021-05-21 21:33:22 +0800

    [update]

--- fetch_src.sh
+++ fetch_src.sh
@@ -6,31 +6,12 @@ echo 'download success'
 tar -zxvf quickjs-2021-03-27.tar.xz 
 echo 'unzip success'
 
-mv quickjs-2021-03-27 quickjs
+cd quickjs-2021-03-27
+
+make
 
 mkdir src
 
-cd quickjs
-
-cp 	quickjs.c \
-	quickjs.h \
-	libregexp.c \
-	libregexp.h \
-	libunicode.c \
-	libunicode.h \
-	cutils.c \
-	cutils.h \
-	quickjs-libc.c \
-	quickjs-libc.h \
-	libbf.c \
-	libbf.h \
-	qjscalc.c \
-	repl.c \
-	qjs.c \
-	libunicode-table.h \
-	list.h \
-	libregexp-opcode.h \
-	quickjs-atom.h \
-	VERSION	  ../src
+cp quickjs/*.c quickjs/*.h ./src
 
 
