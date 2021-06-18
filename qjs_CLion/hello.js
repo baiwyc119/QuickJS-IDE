f622d9618dd04ab27f9da44724d845853f619a2d
benpeng.jbp <benpeng.jbp@alibaba-inc.com>
2021-06-18 10:26:04 +0800

    [update]add vm

--- qjs_CLion/hello.js
+++ qjs_CLion/hello.js
@@ -1,1 +1,2 @@
 console.log("hello word!");
+print("C语言中文网")
---
+++ qjs_CLion/src/qjs-vm.h
@@ -0,0 +1,8 @@
+//
+// Created by benpeng.jiang on 2021/6/18.
+//
+
+#ifndef QJS_CLION_QJS_VM_H
+#define QJS_CLION_QJS_VM_H
+
+#endif //QJS_CLION_QJS_VM_H
